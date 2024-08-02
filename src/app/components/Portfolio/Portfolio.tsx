782de316f1e44147951e17dc36f177c64e2a5482
Kate Roshchupkina <katerina.ermakov@mail.ru>
2024-08-02 16:51:45 +0300

    Fixed some bugs

--- src/app/components/Portfolio/Portfolio.tsx
+++ src/app/components/Portfolio/Portfolio.tsx
@@ -5,13 +5,16 @@ import { TPortfolioCard } from "@/types/types";
 
 import PortfolioCard from "@/components/PortfolioCard/PortfolioCard";
 import { useTranslations } from "next-intl";
+import { RESUME_NAME } from "@/constants";
 
 const Portfolio: FC = () => {
   const t = useTranslations();
 
   return (
     <div className="portfolio" id="portfolio">
-      <h3 className="portfolio__title title-p">Опыт</h3>
+      <h3 className="portfolio__title title-p">
+        {t.raw("PortfolioPage").title}
+      </h3>
       <ul className="portfolio-cards">
         {t
           .raw("Portfolio")
@@ -19,8 +22,13 @@ const Portfolio: FC = () => {
             item.main ? <PortfolioCard key={item.id} {...item} /> : "",
           )}
       </ul>
-      <a className="portfolio__all link-icon" href="#">
-        Посмотреть резюме полностью
+      <a
+        className="portfolio__all link-icon"
+        href={`\${RESUME_NAME}`}
+        download={RESUME_NAME}
+        target="_blank"
+      >
+        {t.raw("PortfolioPage").all}
       </a>
     </div>
   );
